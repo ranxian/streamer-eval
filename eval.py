6ce7b6365ebef32e6eeaa9b6e46ebef754a7392c
Ran Xian <xranthoar@gmail.com>
2016-10-20 21:58:26 -0400

    cpu, mem, fps

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 # eval.py - Evaluate the performance of streamer
 from termcolor import colored
 import subprocess
+from threading import Thread
 from subprocess import call
 from subprocess import Popen
 from argparse import ArgumentParser
@@ -13,41 +14,47 @@ LOCAL_HOST_DIR = "localhost:/Users/xianran/Code/TX1/streamer/build"
 NUC_HOST_DIR = "/Users/xianran/Code/TX1/streamer/build"
 TEGRA_HOST_DIR = "tegra-1:/home/ubuntu/Code/tx1dnn/build"
 
+# Default values
 DEFAULT_NETWORK = "GoogleNet"
+DEFAULT_CAMERA = "GST_TEST"
+DEFAULT_ITERATION = 100
 
 
 class MetricCollector(object):
 
-  def __init__(self, host, on_tegra=False, on_nuc=False):
+  """Collector for cpu, memory and gpu stats"""
+
+  def __init__(self, host, on_tegra=False, on_nuc=False, on_mac=False):
     self.host = host
     self.on_tegra = on_tegra
     self.on_nuc = on_nuc
-    self.sampler_process = None
+    self.on_mac = on_mac
+    self.readout_thread = None
 
-    self.mems = []
-    self.cpus = []
-    self.gpus = []
+    self.mem = 0.0
+    self.cpu = 0.0
+    self.gpu = 0.0
 
-  def execute_command_(self, cmd, wait=True):
+    self.stopped = True
+
+  def get_pid_(self):
+    return self.execute_command_("pidof benchmark")[0]
+
+  def execute_command_(self, cmd):
     ssh_command = "ssh %s 'zsh -l -c \"%s\"'" % (self.host, cmd)
-    if wait:
-      self.sampler_process = Popen(
-          ssh_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    else:
-      call(ssh_command, shell=True)
+    process = Popen(
+        ssh_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    return process.communicate()
 
   def start(self):
-    if self.on_tegra:
-      ssh_command = "sudo pkill -9 -f *tegrastats*"
-      self.execute_command_(ssh_command, wait=True)
-      ssh_command = "sudo ~/tegrastats"
-      self.execute_command_(ssh_command)
-    elif self.on_nuc:
-      pass
-    else:
-      pass
+    self.readout_thread = Thread(target=self.collect_stats_)
+    self.stopped = False
+    self.readout_thread.start()
 
   def process_tegra_(self, line):
+    self.results = []
+
     sp = line.split(" ")
     mem_str = sp[1].split("/")[0]
     cpu_str = sp[5][1:-6]
@@ -62,84 +69,114 @@ class MetricCollector(object):
 
     return mem, cpu, gpu
 
-  def process_(self):
-    for line in self.results:
-      if self.on_tegra:
-        mem, cpu, gpu = self.process_tegra_(line)
-        self.mems.append(mem)
-        self.cpus.append(cpu)
-        self.gpus.append(gpu)
-      elif self.on_nuc:
-        pass
-      else:
-        pass
+  def collect_stats_(self):
+    while not self.stopped:
+      pid = self.get_pid_()
+      if pid == '' or pid == None:
+        continue
+      # Get stats by executing ps aux on pid
+      collect_stats_cmd = "ps aux -q " + pid
+      output, _ = self.execute_command_(collect_stats_cmd)
+
+      # Remove empty strings
+      if output == '':
+        continue
+      output = output.split("\n")[1]
+      if output == '':
+        break
+      output = filter(bool, output.split(" "))
+      if output[-1] == '<defunct>':
+        break
+
+      self.cpu = float(output[2])
+      if output[5] != '0':
+        self.mem = float(output[5])
 
   def stop(self):
-    self.results = []
-    self.sampler_process.terminate()
-    while True:
-      line = self.sampler_process.stdout.readline()
-      if line == None or line == '':
-        break
-      self.results.append(line.strip())
-
-    self.process_()
+    self.stopped = True
+    self.readout_thread.join()
+    self.readout_thread = None
 
   def get_cpu_usage(self):
-    return np.mean(self.cpus)
+    return self.cpu
 
   def get_gpu_usage(self):
-    return np.mean(self.gpus)
+    return self.gpu
 
   def get_mem_usage(self):
-    return np.mean(self.mems)
+    return self.mem
 
 
 def print_error(msg):
+  """Print message in alert color"""
   print colored(msg, 'red')
 
 
 def sync_codebase(host_dir):
-  ssh_command = "rsync -avh --delete --exclude=.git/* --exclude=build/* --exclude=.idea/* --exclude=config/* %s/.. %s/.." % (
+  ssh_command = "rsync -avh --delete --exclude=.git/* --exclude=build/* --exclude=.idea/* --exclude=config/* %s/.. %s/.. > /dev/null" % (
       LOCAL_DIR, host_dir)
   if call(ssh_command, shell=True) != 0:
     print_error("Can't sync codebase with: " + host_dir)
     exit(-1)
 
 
-def remote_execute(host_dir, cmd):
+def remote_execute(host_dir, *cmd):
   host, streamer_dir = host_dir.split(":")
   ssh_command = "ssh %s 'zsh -l -c \"(cd %s; %s)\"'" % (
-      host, streamer_dir, cmd)
-  print ssh_command
-  if call(ssh_command, shell=True) != 0:
+      host, streamer_dir, ' '.join(cmd))
+  p = Popen(
+      ssh_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  output = p.communicate()
+
+  if p.returncode != 0:
     print_error("Error executing: %s on %s" % (host_dir, cmd))
     exit(-1)
+
+  return output
 
 
 def build_streamer(host_dir):
   remote_execute(host_dir, "make -j")
 
 
-def run_experiment(host_dir, network=DEFAULT_NETWORK):
+def run_experiment(host_dir, network=DEFAULT_NETWORK, camera=DEFAULT_CAMERA, on_tegra=False, on_nuc=False):
+  def grab_fps(result, pattern):
+    for line in result.split("\n"):
+      if pattern in line and "fps" in line:
+        return float(line.split(" ")[-1])
+    return 0.0
+
   sync_codebase(host_dir)
   build_streamer(host_dir)
-  remote_execute(host_dir, "apps/benchmark")
+
+  remote_execute(host_dir, "sleep 1")
+
+  host, _ = host_dir.split(":")
+  collector = MetricCollector(host, on_tegra=on_tegra)
+  collector.start()
+
+  output, _ = remote_execute(host_dir,
+                             "GLOG_minloglevel=3 apps/benchmark",
+                             '--experiment', 'classification',
+                             '--camera', camera,
+                             '--net', network,
+                             '--verbose', 'true',
+                             '--iter', str(DEFAULT_ITERATION))
+  collector.stop()
+  print 'cpu', collector.get_cpu_usage()
+  print 'mem', collector.get_mem_usage()
+  print 'fps', grab_fps(output, "classifier")
 
 
 def heter_eval():
-  run_experiment(LOCAL_HOST_DIR)
+  run_experiment(TEGRA_HOST_DIR, on_tegra=True)
+  # run_experiment(LOCAL_HOST_DIR)
 
 
 def heter_plot():
   pass
 
 if __name__ == "__main__":
-  collector = MetricCollector(TEGRA_HOST_DIR.split(":")[0], on_tegra=True)
-  collector.start()
-  time.sleep(4)
-  collector.stop()
-
   parser = ArgumentParser("Evaluate performance of streamer")
   parser.add_argument(
       "--heter_eval", help="Run heterogeneous experiment", action='store_true')
